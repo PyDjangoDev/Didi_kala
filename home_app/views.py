d339943d1d1e7b84969e8a5a9a453651c4d37b1b
AmirHossein <amirrezzaasghari@gmail.com>
2026-07-15 21:15:55 +0430

    makeing amazing part in index

--- home_app/views.py
+++ home_app/views.py
@@ -1,21 +1,31 @@
 from django.shortcuts import render
 from products_app.models import Category
+from products_app.models import Product 
 
 # Create your views here.
 
 def home_app_view(request):
+    amazing_products = Product.objects.filter(
+    off__gt=0, 
+    is_active=True, 
+    is_delete=False
+    ).order_by('-off') 
     return render (request,template_name='home.html',context={
-        
+        'amazing_products' : amazing_products       
     })
     
     
     
     
 def header_component(request):
+
     
     return render(request,template_name='header.html',context={
+
         
     })
+    
+    
     
 def footer_component(request):
     categories = Category.objects.filter(parent=None, is_active=True).prefetch_related('children')  
@@ -24,3 +34,5 @@ def footer_component(request):
         
     })
     
+    
+
--- products_app/urls.py
+++ products_app/urls.py
@@ -1,15 +1,21 @@
+# from django.urls import path
+# from . import views 
+
+
+
+# urlpatterns = [
+#     path('',views.products_app_view, name='products'),
+#     path('detail/<id1>/<slug>/',views.product_detail_view,name= 'id_slug_to_product'),
+#     path('detail/<id1>/',views.product_detail_view,name= 'id_to_product'),
+# ]
+
 from django.urls import path
 from . import views 
 
-
-
 urlpatterns = [
-    path('',views.products_app_view, name='products'),
-    path('detail/<id1>/<slug>/',views.product_detail_view,name= 'id_slug_to_product'),
-    path('detail/<id1>/',views.product_detail_view,name= 'id_to_product'),
+    path('', views.products_app_view, name='products'),
+    # مسیر کامل با id و slug - باید اول باشد
+    path('detail/<int:id1>/<slug:slug>/', views.product_detail_view, name='id_slug_to_product'),
+    # مسیر فقط با id - باید بعد از مسیر کامل بیاید
+    path('detail/<int:id1>/', views.product_detail_view, name='id_to_product'),
 ]
-
-
-
-
-
